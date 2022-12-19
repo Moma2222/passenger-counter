e1993d9f60ec7b2f6ba9aefe595d93493195fcdd
Moma2222 <121061899+Moma2222@users.noreply.github.com>
2022-12-19 23:15:46 +0200

    Update new.js

--- new.js
+++ new.js
@@ -3,18 +3,13 @@ let count = 0
 let saveEl = document.getElementById("save-el")
 
 function increment() {
-    // פונקציה הוספה-סכימה
     count += 1
     countEl.innerText = count
 }
-// פונקציה לשמירה
 function save() {
     let countStr = count + " - "
-    saveEl.innerHTML += countStr
+    saveEl.textContent += countStr
     console.log(count)
-    fanction clearInterval(countEl) {
-        
-    }
-    /*countEl.textContent = 0
-    count = 0*/
+    countEl.textContent = 0
+    count = 0
 }
